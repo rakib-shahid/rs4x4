e15e1bfd7e43d181c0a8e15d0f35b7e0728a550b
rakib-shahid <rakibshahid1298@gmail.com>
2023-09-13 13:21:00 -0400

    vial compatible

---
+++ qmk code/keymaps/vial/rules.mk
@@ -0,0 +1,12 @@
+# This file intentionally left blank
+# MCU name
+MCU = RP2040
+
+# Bootloader selection
+BOOTLOADER = rp2040
+
+OLED_ENABLE = yes
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
