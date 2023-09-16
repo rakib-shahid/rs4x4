567842233f24b9586a788e2e9236d8c262458fe5
rakib-shahid <rakibshahid1298@gmail.com>
2023-09-15 23:01:13 -0400

    4x4

--- qmk code/keymaps/vial/keymap.c
+++ qmk code/keymaps/vial/keymap.c
@@ -193,7 +193,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ 0 │ . │Ent│ + │
      * └───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_1x1(
-        KC_F
+    [0] = LAYOUT_ortho_4x4(
+        KC_KP_7,   KC_KP_8,   KC_KP_9,   KC_KP_MINUS,
+        KC_KP_4,   KC_KP_5,   KC_KP_6,   KC_KP_PLUS,
+        KC_KP_1,   KC_KP_2,   KC_KP_3,   KC_KP_ASTERISK,
+        KC_KP_0,   KC_X, KC_KP_DOT, KC_KP_SLASH
     )
 };
