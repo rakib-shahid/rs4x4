1761a79b56a36d6f28b713b14e5075acdcdd4930
rakib-shahid <rakibshahid1298@gmail.com>
2023-09-20 19:43:17 -0400

    fixed for 4x4

--- qmk code/keymaps/vial/keymap.c
+++ qmk code/keymaps/vial/keymap.c
@@ -169,10 +169,11 @@ static void render_animation(void){
 bool process_record_user(uint16_t keycode, keyrecord_t * record) {
     if (record->event.pressed) {
         render_animation();
+        sethsv(HSV_WHITE, (LED_TYPE *)&led[0]);
+        rgblight_set();
     }
     return true;
 }
-
 bool oled_task_user(void) {
     
     // oled_write_P(PSTR("Hello world"), false);
@@ -197,6 +198,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_KP_7,   KC_KP_8,   KC_KP_9,   KC_KP_MINUS,
         KC_KP_4,   KC_KP_5,   KC_KP_6,   KC_KP_PLUS,
         KC_KP_1,   KC_KP_2,   KC_KP_3,   KC_KP_ASTERISK,
-        KC_KP_0,   KC_X, KC_KP_DOT, KC_KP_SLASH
+        KC_MEDIA_PLAY_PAUSE,   KC_KP_0, KC_KP_DOT, KC_KP_SLASH
     )
 };
