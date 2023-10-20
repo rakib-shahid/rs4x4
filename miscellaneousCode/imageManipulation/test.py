f341267fccfa0a84420ca90ff9aef90d281b2fde
rakib-shahid <rakibshahid1298@gmail.com>
2023-10-20 01:17:45 -0400

    remove tracked files

--- miscellaneousCode/imageManipulation/test.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import os
-
-# Open the image
-for i in range(1,3):
-    image = Image.open(f'image/frames/{i}.bmp')
-    image = image.convert('1')
-    image.save("image\\frames\\"+str(i)+"bw.bmp", lossless = True)
--- miscellaneousCode/qmk code/spi qmk/testinggif/mcuconf.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include_next <mcuconf.h>
-#undef RP_SPI_USE_SPI0
-#define RP_SPI_USE_SPI0 TRUE
