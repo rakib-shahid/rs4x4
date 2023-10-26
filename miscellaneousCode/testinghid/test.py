ca2a4466f1cea9d0cca8fd64eb08fc4c77b77627
rakib-shahid <rakibshahid1298@gmail.com>
2023-10-26 00:01:28 -0400

    added cpu gpu temps

--- miscellaneousCode/testinghid/test.py
+++ miscellaneousCode/testinghid/test.py
@@ -1,6 +1,7 @@
 import requests
 import apikey
 import time
+import wmi
 
 user = 'rshahid10'
 key = apikey.key
@@ -11,12 +12,15 @@ lastString = "Not playing anything"
 outString = "Not playing anything"
 nowplaying = False
 
-while True:
+def getCurrentSong(url, lastString, outString, nowplaying):
     response = requests.get(url)
     data = response.json()
     nowplaying = None
-    trackName = data['recenttracks']['track'][0]['name']
-    artistName = data['recenttracks']['track'][0]['artist']['#text']
+    try:
+        trackName = data['recenttracks']['track'][0]['name']
+        artistName = data['recenttracks']['track'][0]['artist']['#text']
+    except KeyError:
+        print("KeyError encountered, idk what to do lol")
     try:
         if data['recenttracks']['track'][0]['@attr']['nowplaying'] == 'true':
             nowplaying = True
@@ -32,6 +36,34 @@ while True:
         # check if state changed, if changed print
         if outString != lastString:
             print(outString)
-    lastString = outString
-    time.sleep(1)
+    return lastString, outString, nowplaying
+
+def getTemps():
+    cpuTemp = 0
+    gpuTemp = 0
+    w = wmi.WMI(namespace="root\OpenHardwareMonitor")
+    temperature_infos = w.Sensor()
+    for sensor in temperature_infos:
+        
+        if sensor.SensorType==u'Temperature':
+            if "cpu package" in (sensor.Name).lower():
+                print("CPU")
+                print(sensor.Value)
+                cpuTemp = sensor.Value
+            if "gpu core" in (sensor.Name).lower():
+                print("GPU")
+                print(sensor.Value)
+                gpuTemp = sensor.Value
+    return cpuTemp,gpuTemp
+
+
+getTemps()
+while True:
+    # lastString, outString, nowplaying = getCurrentSong(url,lastString,outString,nowplaying)
+    # lastString = outString
+    getTemps()
+    
+    # time.sleep(1)
+    
+
         
