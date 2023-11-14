c8673aafbaf352cca5cfece791b9061758dd54ac
rakib-shahid <rakibshahid1298@gmail.com>
2023-11-13 23:46:28 -0500

    hid image

---
+++ miscellaneousCode/imageThruHID/keymaps/vial/rules.mk
@@ -0,0 +1,19 @@
+# This file intentionally left blank
+# MCU name
+MCU = RP2040
+
+# Bootloader selection
+BOOTLOADER = rp2040
+
+RAW_ENABLE = yes
+OLED_ENABLE = yes
+WPM_ENABLE = yes
+
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+WS2812_DRIVER = vendor
+RGBLIGHT_ENABLE = yes
+
+CONSOLE_ENABLE = yes
+
