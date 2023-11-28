0d87cd9788e7057337e827d3b698caf4075085c4
rakib-shahid <rakibshahid1298@gmail.com>
2023-11-28 13:31:16 -0500

    cleaning up hid

---
+++ miscellaneousCode/finalHID/main.py
@@ -0,0 +1,30 @@
+import spotify_api as sp
+import time
+# print(sp.getCurrentTrack())
+
+track_info = {
+    "is_playing": False,
+    "track_name": '',
+    "artist_name": '',
+    "image_url": ''
+}
+
+def updateTrackInfo(token):
+    current_track_info = sp.getCurrentTrack(token)
+    if (current_track_info != None):
+        track_info["is_playing"] = current_track_info["is_playing"]
+        track_info["track_name"] = current_track_info["item"]["name"]
+        track_info["artist_name"] = current_track_info["item"]["artists"][0]["name"]
+        track_info["image_url"] = current_track_info["item"]["album"]["images"][2]["url"]
+    else:
+        track_info["is_playing"] = False
+        track_info["track_name"] = ""
+        track_info["artist_name"] = ""
+        track_info["image_url"] = ""
+
+token = sp.getToken()
+print(f"track info = {track_info}")
+while True:
+    updateTrackInfo(token)
+    print(f"updated track info = {track_info}")
+    time.sleep(0.1)
---
+++ miscellaneousCode/finalHID/spotify_api.py
@@ -0,0 +1,93 @@
+import spotifykeys
+import requests, webbrowser, json,time
+from requests_oauthlib import OAuth2Session
+from requests.auth import HTTPBasicAuth
+
+scope = 'user-read-currently-playing'
+auth_url = "https://accounts.spotify.com/authorize"
+token_url = "https://accounts.spotify.com/api/token"
+client_id = spotifykeys.client_id
+client_secret = spotifykeys.client_secret
+redirect_uri = spotifykeys.redirect_uri
+
+spotify = OAuth2Session(client_id, scope=scope, redirect_uri=redirect_uri)
+
+def readToken():
+    f = open("test.cache", "r")
+    token_content = f.read()
+    if len(token_content) != 0:
+        token = json.loads(token_content)
+        return token['access_token']
+    else:
+        raise FileNotFoundError
+
+def writeToken(token):
+    f = open("test.cache", "w")
+    f.write(str(token).replace("'", '"'))
+    f.close()
+
+# gets token locally or new one online
+def getToken():
+    token = None
+    try:
+        token = readToken()
+    except:
+        authorization_url, state = spotify.authorization_url(auth_url)
+        print('Please go here and authorize: ', authorization_url)
+        webbrowser.open(authorization_url)
+        redirect_response = input('\n\nPaste the full redirect URL here: ')
+
+        auth = HTTPBasicAuth(client_id, client_secret)
+        token = spotify.fetch_token(token_url, auth=auth, authorization_response=redirect_response)
+        writeToken(token)
+        token = token['access_token']
+    finally:
+        # print(token)
+        return token
+        
+# returns json of api response
+def getCurrentTrack(token):
+    out = None
+    start = time.process_time()
+    url = 'https://api.spotify.com/v1/me/player/currently-playing/'
+    headers = {
+        'Authorization':f'Bearer {token}'
+    }
+    response = requests.get(url,headers=headers)
+    try:
+        out = json.loads(response.content)
+        return out
+    except Exception as e:
+        print("Not playing anything")
+    finally:
+        print("Total time taken to getCurrentTrack = "+ str(time.process_time() - start))
+        return out
+
+    
+
+# track_info = {
+#     "is_playing": False,
+#     "track_name": '',
+#     "artist_name": '',
+#     "image_url": ''
+# }
+
+# def updateTrackInfo(token):
+#     current_track_info = getCurrentTrack(token)
+#     if (current_track_info != None):
+#         track_info["is_playing"] = current_track_info["is_playing"]
+#         track_info["track_name"] = current_track_info["item"]["name"]
+#         track_info["artist_name"] = current_track_info["item"]["artists"][0]["name"]
+#         track_info["image_url"] = current_track_info["item"]["album"]["images"][2]["url"]
+#     else:
+#         track_info["is_playing"] = False
+#         track_info["track_name"] = ""
+#         track_info["artist_name"] = ""
+#         track_info["image_url"] = ""
+
+# token = getToken()
+# print(f"track info = {track_info}")
+# while True:
+#     updateTrackInfo(token)
+#     print(f"updated track info = {track_info}")
+#     time.sleep(0.33)
